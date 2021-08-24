35dacde072b2e4eac1e879e391d267e4a3ca6800
Christian Gorenflo <christian@axelar.network>
2021-08-24 17:10:23 -0400

    chore: copy files from axelar-core

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/axelarnetwork/utils
+
+go 1.16
+
+require (
+	github.com/cosmos/cosmos-sdk v0.42.9
+	github.com/ethereum/go-ethereum v1.10.8
+	github.com/stretchr/testify v1.7.0
+	github.com/tendermint/tendermint v0.34.11
+)
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
---
+++ test/utils.go
@@ -0,0 +1,45 @@
+// Package testutils provides general purpose utility functions for unit/integration testing.
+package testutils
+
+import (
+	"fmt"
+	"testing"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// Func wraps a regular testing function so it can be used as a pointer function receiver
+type Func func(t *testing.T)
+
+// Repeat executes the testing function n times
+func (f Func) Repeat(n int) Func {
+	return func(t *testing.T) {
+		for i := 0; i < n; i++ {
+			f(t)
+		}
+	}
+}
+
+// Events wraps sdk.Events
+type Events []abci.Event
+
+// Filter returns a collection of events filtered by the predicate
+func (fe Events) Filter(predicate func(events abci.Event) bool) Events {
+	var filtered Events
+	for _, event := range fe {
+		if predicate(event) {
+			filtered = append(filtered, event)
+		}
+	}
+	return filtered
+}
+
+// ErrorCache is a struct that can be used to get at the error that is emitted by test assertions when passing it instead ot *testing.T
+type ErrorCache struct {
+	Error error
+}
+
+// Errorf records the given formatted string as an erro
+func (ec *ErrorCache) Errorf(format string, args ...interface{}) {
+	ec.Error = fmt.Errorf(format, args...)
+}
