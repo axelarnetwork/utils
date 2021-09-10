cb00e13440a29b32616899ae35496b68ec470658
Wil Collins <wil.collins@gmail.com>
2021-09-10 09:27:36 -0700

    feat(test): set file contents

--- test/utils.go
+++ test/utils.go
@@ -3,7 +3,12 @@ package testutils
 
 import (
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -52,4 +57,76 @@ func SetEnv(t *testing.T, key string, val string) {
 	orig := os.Getenv(key)
 	os.Setenv(key, val)
 	t.Cleanup(func() { os.Setenv(key, orig) })
+}
+
+// SetFileContents safely sets the contents of the specified filepath to the specified value, then resets it to the original value upon test closure
+func SetFileContents(t *testing.T, filepath string, contents string) {
+	SetFileContentsAndPermissions(t, filepath, contents, 0644)
+}
+
+func writeFile(filepath string, contents []byte, perm fs.FileMode) {
+	err := ioutil.WriteFile(resolve(filepath), contents, perm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(filepath string) []byte {
+	contents, err := ioutil.ReadFile(resolve(filepath))
+	if err != nil {
+		panic(err)
+	}
+	return contents
+}
+
+func resolve(path string) string {
+	path = resolveHome(path)
+	path = resolveCurrentDirectory(path)
+	return path
+}
+
+func resolveCurrentDirectory(path string) string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	if path == "." {
+		path = wd
+	} else if strings.HasPrefix(path, "./") {
+		path = filepath.Join(wd, path[2:])
+	}
+	return path
+}
+
+func resolveHome(path string) string {
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+	if path == "~" {
+		path = dir
+	} else if strings.HasPrefix(path, "~/") {
+		path = filepath.Join(dir, path[2:])
+	}
+	return path
+}
+
+// SetFileContentsAndPermissions safely sets the contents of the specified filepath to the specified value and FileMode, then resets it to the original value upon test closure
+func SetFileContentsAndPermissions(t *testing.T, path string, contents string, perm fs.FileMode) {
+	os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+	}
+
+	original := readFile(path)
+	writeFile(path, []byte(contents), perm)
+	// this may be overkill to reset the file to its original value
+	t.Cleanup(
+		func() {
+			writeFile(path, original, perm)
+		},
+	)
 }
