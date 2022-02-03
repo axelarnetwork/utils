db219ced5a6d6f05e7d5d9bdfc1f8d98b51c5ae5
Christian Gorenflo <christian@axelar.network>
2022-02-03 14:43:16 -0500

    feat(jobs): make jobs more usable and cancellable

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -1,35 +1,78 @@
 package jobs
 
-import "sync"
+import (
+	"context"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
+)
 
 // Job represents a (long-running) process that can be spawned on a separate go-routine.
 // When encountering an error, a Job should send that error to the given channel and continue to run.
-type Job func(chan<- error)
+type Job func(ctx context.Context) error
 
 // JobManager manages multiple concurrent jobs and handles their errors. Can wait for all jobs and error handling to finish.
 type JobManager struct {
-	wgJobs  *sync.WaitGroup
-	wgErr   *sync.WaitGroup
-	errChan chan error
+	wgJobs      *sync.WaitGroup
+	done        chan struct{}
+	errChan     chan error
+	ctx         context.Context
+	once        *sync.Once
+	jobCapacity *capacityMgr
+}
+
+type capacityMgr struct {
+	semaphore *semaphore.Weighted
+}
+
+func (mgr *capacityMgr) Acquire(ctx context.Context, n int64) error {
+	if mgr.semaphore != nil {
+		return mgr.semaphore.Acquire(ctx, n)
+	}
+	return nil
+}
+
+func (mgr *capacityMgr) Release(n int64) {
+	if mgr.semaphore != nil {
+		mgr.semaphore.Release(n)
+	}
+}
+
+// MgrOptions modify the behaviour of the JobManager
+type MgrOptions func(*JobManager) *JobManager
+
+// WithMaxCapacity defines how many jobs will be run in parallel
+func WithMaxCapacity(cap int64) MgrOptions {
+	return func(mgr *JobManager) *JobManager {
+		mgr.jobCapacity = &capacityMgr{semaphore.NewWeighted(cap)}
+		return mgr
+	}
+}
+
+// WithErrorCacheCapacity defines the size of the error cache. If the cache is full new errors from jobs will be ignored. Default is 1000
+func WithErrorCacheCapacity(cap int64) MgrOptions {
+	return func(mgr *JobManager) *JobManager {
+		mgr.errChan = make(chan error, cap)
+		return mgr
+	}
 }
 
 // NewMgr returns a new JobManager
-func NewMgr(errHandler func(err error)) *JobManager {
-	wgErr := &sync.WaitGroup{}
-	wgErr.Add(1)
-	errChan := make(chan error, 1000)
-	go func() {
-		defer wgErr.Done()
-		for err := range errChan {
-			errHandler(err)
-		}
-	}()
-
-	return &JobManager{
-		wgErr:   wgErr,
-		errChan: errChan,
-		wgJobs:  &sync.WaitGroup{},
+func NewMgr(ctx context.Context, opts ...MgrOptions) *JobManager {
+	mgr := &JobManager{
+		ctx:         ctx,
+		jobCapacity: &capacityMgr{},
+		done:        make(chan struct{}),
+		once:        &sync.Once{},
+		errChan:     make(chan error, 1000),
+		wgJobs:      &sync.WaitGroup{},
 	}
+
+	for _, opt := range opts {
+		mgr = opt(mgr)
+	}
+
+	return mgr
 }
 
 // AddJobs calls AddJob for each of the given jobs
@@ -41,16 +84,45 @@ func (mgr *JobManager) AddJobs(jobs ...Job) {
 
 // AddJob spawns a new goroutine for the given job, manages its lifetime and handles its errors
 func (mgr *JobManager) AddJob(j Job) {
-	mgr.wgJobs.Add(1)
 	go func() {
-		defer mgr.wgJobs.Done()
-		j(mgr.errChan)
+		mgr.wgJobs.Add(1)
+		if err := mgr.jobCapacity.Acquire(mgr.ctx, 1); err != nil {
+			mgr.tryCacheError(err)
+		}
+		go func() {
+			defer mgr.wgJobs.Done()
+			defer mgr.jobCapacity.Release(1)
+			if err := j(mgr.ctx); err != nil {
+				mgr.tryCacheError(err)
+			}
+		}()
 	}()
 }
 
-// Wait blocks until all jobs and error handling have finished
-func (mgr *JobManager) Wait() {
-	mgr.wgJobs.Wait()
-	close(mgr.errChan)
-	mgr.wgErr.Wait()
+func (mgr *JobManager) tryCacheError(err error) {
+	// do not block if the error queue is already full
+	select {
+	case mgr.errChan <- err:
+		break
+	default:
+		break
+	}
+}
+
+// Done returns a channel that gets closed when all jobs finished
+func (mgr *JobManager) Done() <-chan struct{} {
+	go func() {
+		mgr.once.Do(func() {
+			mgr.wgJobs.Wait()
+			close(mgr.errChan)
+			close(mgr.done)
+		})
+	}()
+
+	return mgr.done
+}
+
+// Errs returns errors encountered during job execution
+func (mgr *JobManager) Errs() <-chan error {
+	return mgr.errChan
 }
--- jobs/jobs_test.go
+++ jobs/jobs_test.go
@@ -1,43 +1,104 @@
-package jobs
+package jobs_test
 
 import (
+	"context"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/axelarnetwork/utils/jobs"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/axelarnetwork/utils/test/rand"
 )
 
-func TestJobManager_Wait(t *testing.T) {
+func TestJobManager_Errs(t *testing.T) {
 	// setup
 	jobCount := rand.I64Between(0, 100)
-	var jobs []Job
-	var expectedErrCount int64
+	mgr := jobs.NewMgr(context.Background())
 	for i := int64(0); i < jobCount; i++ {
-		errCount := rand.I64Between(0, 10000)
-		expectedErrCount += errCount
-		job := randomJob(errCount, i)
-		jobs = append(jobs, job)
+		job := randomJob(i)
+		mgr.AddJob(job)
 	}
-	var actualErrCount int64
-	errHandler := func(err error) { actualErrCount++ }
-	mgr := NewMgr(errHandler)
-
 	// test
-	mgr.AddJobs(jobs...)
-	mgr.Wait()
-	assert.Equal(t, expectedErrCount, actualErrCount)
+	<-mgr.Done()
+	assert.Len(t, mgr.Errs(), int(jobCount))
+}
+
+func TestJobManager_ContextCancelled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	mgr := jobs.NewMgr(ctx)
+
+	jobCount := rand.I64Between(0, 100)
+	for i := 0; i < int(jobCount); i++ {
+		mgr.AddJob(func(ctx context.Context) error {
+			<-ctx.Done()
+			return nil
+		})
+	}
+
+	select {
+	case <-mgr.Done():
+		assert.Fail(t, "it should be impossible for the mgr to be done here")
+	default:
+		break
+	}
+
+	cancel()
+
+	timeout, timeoutCancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer timeoutCancel()
+
+	select {
+	case <-mgr.Done():
+		break
+	case <-timeout.Done():
+		assert.Fail(t, "timed out")
+	}
+}
+
+func TestJobManager_MoreJobsThanCap(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cap := rand.I64Between(1, 20)
+	mgr := jobs.NewMgr(ctx, jobs.WithMaxCapacity(cap))
+
+	jobCount := rand.I64Between(cap, 100)
+	blockingCtx, cancel := context.WithCancel(context.Background())
+	jobsStarted := int64(0)
+	for i := 0; i < int(jobCount); i++ {
+		mgr.AddJob(func(context.Context) error {
+			atomic.AddInt64(&jobsStarted, 1)
+			<-blockingCtx.Done()
+			return nil
+		})
+	}
+
+	// only jobs up to the cap have started because no jobs have finished yet
+	assert.Equal(t, cap, jobsStarted)
+	cancel()
+
+	timeout, timeoutCancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer timeoutCancel()
+
+	select {
+	case <-mgr.Done():
+		break
+	case <-timeout.Done():
+		assert.Fail(t, "timed out")
+	}
+
+	// now all jobs must have finished
+	assert.Equal(t, jobCount, jobsStarted)
 }
 
 // this extracted function is needed to close over the loop counter i
-func randomJob(errCount int64, i int64) Job {
-	return func(e chan<- error) {
-		for j := int64(0); j < errCount; j++ {
-			e <- fmt.Errorf("error %d by job %d", j, i)
-		}
+func randomJob(i int64) jobs.Job {
+	return func(ctx context.Context) error {
 		duration := time.Duration(rand.I64Between(0, 100)) * time.Millisecond
 		time.Sleep(duration)
+		return fmt.Errorf("error by job %d", i)
 	}
 }
