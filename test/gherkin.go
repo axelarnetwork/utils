1caef0792ada884d628b4aa65039c2ba91110f2f
Christian Gorenflo <christian@axelar.network>
2022-04-01 00:39:14 -0400

    feat: enhance gherkin framework capabilities

--- test/gherkin.go
+++ test/gherkin.go
@@ -3,6 +3,8 @@ package testutils
 import (
 	"strings"
 	"testing"
+
+	"github.com/axelarnetwork/utils/slices"
 )
 
 const (
@@ -12,195 +14,161 @@ const (
 	_and   = "AND"
 )
 
-// GivenStatement is used to set up unit test preconditions - do not implement yourself
-type GivenStatement interface {
-	Given(description string, setup func(t *testing.T)) WhenStatement
-}
-
-// WhenStatement is used to hit a trigger - do not implement yourself
-type WhenStatement interface {
-	And() GivenStatement
-	Branch(...Runner) Runner
-	When(description string, setup func(t *testing.T)) ThenStatement
-}
-
-// ThenStatement is used to check the test outcome - do not implement yourself
-type ThenStatement interface {
-	And() WhenStatement
-	Branch(...Runner) Runner
-	Then(description string, execution func(t *testing.T)) Runner
-}
-
-// ForEachStatement is used to create a set of tests for each of the test case items
-type ForEachStatement[T any] interface {
-	ForEach(createRunner func(testCase T) Runner) Runner
-}
-
-// Runner executes the test - do not implement yourself
-type Runner interface {
-	Run(t *testing.T, repeats ...int) bool
-}
-
-type given struct {
+// GivenStatement is used to set up unit test preconditions
+type GivenStatement struct {
 	label []string
-	test  []func(t *testing.T)
+	test  func()
 }
 
-type when struct {
+// WhenStatement is used to define conditions under test
+type WhenStatement struct {
 	label []string
-	test  []func(t *testing.T)
+	test  func()
 }
 
-type then struct {
+type thenStatement struct {
 	label []string
-	test  []func(t *testing.T)
+	test  func(t *testing.T)
 }
 
-type testCases[T any] struct {
-	items []T
-}
-
-type multiRunner struct {
-	runners []runner
-}
-
-type runner struct {
-	label []string
-	test  []func(t *testing.T)
-}
+// ThenStatements are used to define test outcomes
+type ThenStatements []thenStatement
 
 // Given starts the test with the first precondition
-func Given(description string, setup func(t *testing.T)) WhenStatement {
-	return when{
+func Given(description string, setup func()) GivenStatement {
+	return GivenStatement{
 		label: []string{_given, description},
-		test:  []func(t *testing.T){setup},
+		test:  setup,
 	}
 }
 
 // When is an independent trigger that can be used to start a statement in a Branch
-func When(description string, setup func(t *testing.T)) ThenStatement {
-	return then{
+func When(description string, setup func()) WhenStatement {
+	return WhenStatement{
 		label: []string{_when, description},
-		test:  []func(t *testing.T){setup},
+		test:  setup,
 	}
 }
 
 // Then is an independent outcome check that can be used to start a statement in a Branch
-func Then(description string, setup func(t *testing.T)) Runner {
-	return runner{
+func Then(description string, setup func(t *testing.T)) ThenStatements {
+	return []thenStatement{{
 		label: []string{_then, description},
-		test:  []func(t *testing.T){setup},
-	}
-}
-
-// TestCases takes an array of arbitrary items to then generate a test for each of the test case items
-func TestCases[T any](items []T) ForEachStatement[T] {
-	return testCases[T]{
-		items: items,
-	}
+		test:  setup,
+	}}
 }
 
 // Given adds an additional precondition
-func (g given) Given(description string, setup func(t *testing.T)) WhenStatement {
-	return when{
-		label: append(g.label, _given, description),
-		test:  append(g.test, setup),
+func (g GivenStatement) Given(description string, setup func()) GivenStatement {
+	return GivenStatement{
+		label: append(g.label, _and, _given, description),
+		test:  func() { g.test(); setup() },
 	}
 }
 
-// And allows to concatenate an additional precondition
-func (w when) And() GivenStatement {
-	return given{
-		label: append(w.label, _and),
-		test:  w.test,
+func (g GivenStatement) Given2(g2 GivenStatement) GivenStatement {
+	return GivenStatement{
+		label: mergeLabels(g.label, g2.label),
+		test:  func() { g.test(); g2.test() },
 	}
 }
 
 // When adds a trigger to the test path
-func (w when) When(description string, setup func(t *testing.T)) ThenStatement {
-	return then{
-		label: append(w.label, _when, description),
-		test:  append(w.test, setup),
+func (g GivenStatement) When(description string, setup func()) WhenStatement {
+	return WhenStatement{
+		label: append(g.label, _when, description),
+		test:  func() { g.test(); setup() },
+	}
+}
+
+func (g GivenStatement) When2(w WhenStatement) WhenStatement {
+	return WhenStatement{
+		label: mergeLabels(g.label, w.label),
+		test:  func() { g.test(); w.test() },
 	}
 }
 
 // Branch allows test branching by adding multiple sub-statements after a Given
-func (w when) Branch(tests ...Runner) Runner {
-	checkFirstWordOfLabels(tests, assertNotTHEN)
-	return branch(w.label, w.test, tests)
+func (g GivenStatement) Branch(thens ...ThenStatements) ThenStatements {
+	slices.ForEach(thens, func(thens ThenStatements) { checkFirstWordOfLabels(thens, assertNotTHEN) })
+
+	out := ThenStatements{}
+	for _, then := range thens {
+		for _, statement := range then {
+			statement := statement
+			out = append(out, thenStatement{
+				label: mergeLabels(g.label, statement.label),
+				test:  func(t *testing.T) { g.test(); statement.test(t) },
+			})
+		}
+	}
+
+	return out
 }
 
 // Branch allows test branching by adding multiple sub-statements after a When
-func (t then) Branch(tests ...Runner) Runner {
-	checkFirstWordOfLabels(tests, assertNotGIVEN)
-	return branch(t.label, t.test, tests)
+func (w WhenStatement) Branch(thens ...ThenStatements) ThenStatements {
+	slices.ForEach(thens, func(thens ThenStatements) { checkFirstWordOfLabels(thens, assertNotGIVEN) })
+
+	out := ThenStatements{}
+	for _, then := range thens {
+		for _, statement := range then {
+			statement := statement
+			out = append(out, thenStatement{
+				label: mergeLabels(w.label, statement.label),
+				test:  func(t *testing.T) { w.test(); statement.test(t) },
+			})
+		}
+	}
+
+	return out
 }
 
-// And allows to concatenate an additional trigger
-func (t then) And() WhenStatement {
-	return when{
-		label: append(t.label, _and),
-		test:  t.test,
+// When adds a trigger to the test path
+func (w WhenStatement) When(description string, setup func()) WhenStatement {
+	return WhenStatement{
+		label: append(w.label, _and, _when, description),
+		test:  func() { w.test(); setup() },
+	}
+}
+
+func (w WhenStatement) When2(w2 WhenStatement) WhenStatement {
+	return WhenStatement{
+		label: mergeLabels(w.label, w2.label),
+		test:  func() { w.test(); w2.test() },
 	}
 }
 
 // Then adds an outcome check to the test path
-func (t then) Then(description string, execution func(t *testing.T)) Runner {
-	return runner{
-		label: append(t.label, _then, description),
-		test:  append(t.test, execution),
-	}
+func (w WhenStatement) Then(description string, execution func(t *testing.T)) ThenStatements {
+	return []thenStatement{{
+		label: append(w.label, _then, description),
+		test:  func(t *testing.T) { w.test(); execution(t) },
+	}}
 }
 
-// ForEach generates a test for each test case
-func (tc testCases[T]) ForEach(createRunner func(testCase T) Runner) Runner {
-
-	runners := make([]Runner, len(tc.items))
-
-	for i, v := range tc.items {
-		runners[i] = createRunner(v)
-	}
-
-	return branch([]string{}, []func(t *testing.T){}, runners)
-}
-
-// Run executes all defined test paths. Optionally, each path is repeated a given number of times
-func (r multiRunner) Run(t *testing.T, repeats ...int) bool {
-	result := true
-	for _, runner := range r.runners {
-		// cannot inline this because if result is false the second part of && is not going to be evaluated
-		newResult := runner.Run(t, repeats...)
-		result = result && newResult
-	}
-	return result
+// Then2 allows the use of a previously defined "then" statement
+func (w WhenStatement) Then2(then ThenStatements) ThenStatements {
+	return slices.Map(then, func(then thenStatement) thenStatement {
+		return thenStatement{
+			label: mergeLabels(w.label, then.label),
+			test:  func(t *testing.T) { w.test(); then.test(t) },
+		}
+	})
 }
 
 // Run executes all defined test paths. Optionally, each path is repeated a given number of times
-func (r runner) Run(t *testing.T, repeats ...int) bool {
+func (then thenStatement) Run(t *testing.T, repeats ...int) bool {
 	repeat := 1
 	if len(repeats) == 1 && repeats[0] > 1 {
 		repeat = repeats[0]
 	}
-	return t.Run(strings.Join(r.label, " "), Func(func(t *testing.T) {
-		for _, f := range r.test {
-			f(t)
-		}
-	}).Repeat(repeat))
+	return t.Run(strings.Join(then.label, " "), Func(then.test).Repeat(repeat))
 }
 
-func checkFirstWordOfLabels(tests []Runner, assertion func(string)) {
-	for _, test := range tests {
-		switch test := test.(type) {
-		case runner:
-			assertion(test.label[0])
-		case multiRunner:
-			for _, r := range test.runners {
-				assertion(r.label[0])
-			}
-		default:
-			panic("do not extend the test suite with custom types")
-		}
-	}
+// Run executes all defined test paths. Optionally, each path is repeated a given number of times
+func (thens ThenStatements) Run(t *testing.T, repeats ...int) bool {
+	return slices.Reduce(thens, true, func(result bool, then thenStatement) bool { return then.Run(t, repeats...) })
 }
 
 func assertNotGIVEN(label string) {
@@ -215,27 +183,9 @@ func assertNotTHEN(label string) {
 	}
 }
 
-func branch(startLabel []string, startTest []func(t *testing.T), tests []Runner) Runner {
-	var mr multiRunner
+func checkFirstWordOfLabels(tests []thenStatement, assertion func(string)) {
 	for _, test := range tests {
-		switch test := test.(type) {
-		case runner:
-			mr.runners = append(mr.runners, concatRunner(startLabel, startTest, test))
-		case multiRunner:
-			for _, r := range test.runners {
-				mr.runners = append(mr.runners, concatRunner(startLabel, startTest, r))
-			}
-		default:
-			panic("do not extend the test suite with custom types")
-		}
-	}
-	return mr
-}
-
-func concatRunner(startLabel []string, startTest []func(t *testing.T), r runner) runner {
-	return runner{
-		label: mergeLabels(startLabel, r.label),
-		test:  append(startTest, r.test...),
+		assertion(test.label[0])
 	}
 }
 
--- test/gherkin_test.go
+++ test/gherkin_test.go
@@ -5,21 +5,22 @@ import (
 	"strings"
 	"testing"
 
-	. "github.com/axelarnetwork/utils/test"
 	"github.com/stretchr/testify/assert"
+
+	. "github.com/axelarnetwork/utils/test"
 )
 
 func TestGherkinSyntax(t *testing.T) {
 	var testLabel string
 	var testPaths int
-	testSetup := Given("some setup", func(t *testing.T) { testLabel = "GIVEN some setup" }).
+	testSetup := Given("some setup", func() { testLabel = "GIVEN some setup" }).
 		Branch(
-			Given("additional setup", func(t *testing.T) { testLabel += " AND GIVEN additional setup" }).
-				And().Given("even more setup", func(t *testing.T) { testLabel += " AND GIVEN even more setup" }).
-				When("a trigger happens", func(t *testing.T) { testLabel += " WHEN a trigger happens" }).
+			Given("additional setup", func() { testLabel += " AND GIVEN additional setup" }).
+				Given("even more setup", func() { testLabel += " AND GIVEN even more setup" }).
+				When("a trigger happens", func() { testLabel += " WHEN a trigger happens" }).
 				Branch(
-					When("a second trigger happens", func(t *testing.T) { testLabel += " AND WHEN a second trigger happens" }).
-						And().When("a third trigger happens", func(t *testing.T) { testLabel += " AND WHEN a third trigger happens" }).
+					When("a second trigger happens", func() { testLabel += " AND WHEN a second trigger happens" }).
+						When("a third trigger happens", func() { testLabel += " AND WHEN a third trigger happens" }).
 						Then("we finally check the outcome", func(t *testing.T) {
 							testLabel += " THEN we finally check the outcome"
 							assertNameEquals(t, testLabel)
@@ -31,7 +32,7 @@ func TestGherkinSyntax(t *testing.T) {
 						testPaths++
 					}),
 				),
-			When("we directly hit the trigger", func(t *testing.T) { testLabel += " WHEN we directly hit the trigger" }).
+			When("we directly hit the trigger", func() { testLabel += " WHEN we directly hit the trigger" }).
 				Then("we check the outcome even earlier", func(t *testing.T) {
 					testLabel += " THEN we check the outcome even earlier"
 					assertNameEquals(t, testLabel)
@@ -59,11 +60,11 @@ func TestGherkinSyntax(t *testing.T) {
 
 func TestGherkinPanicsGIVENAfterWHEN(t *testing.T) {
 	assert.Panics(t, func() {
-		Given("precondition", func(*testing.T) {}).
-			When("trigger", func(*testing.T) {}).
+		Given("precondition", func() {}).
+			When("trigger", func() {}).
 			Branch(
-				Given("forbidden statement", func(*testing.T) {}).
-					When("trigger", func(*testing.T) {}).
+				Given("forbidden statement", func() {}).
+					When("trigger", func() {}).
 					Then("check", func(*testing.T) {}),
 			)
 	})
@@ -71,11 +72,33 @@ func TestGherkinPanicsGIVENAfterWHEN(t *testing.T) {
 
 func TestGherkinPanicsTHENAfterGIVEN(t *testing.T) {
 	assert.Panics(t, func() {
-		Given("precondition", func(*testing.T) {}).
+		Given("precondition", func() {}).
 			Branch(
 				Then("check", func(*testing.T) {}),
 			)
 	})
+}
+
+func TestGherkinSeparateStatements(t *testing.T) {
+	var testLabel string
+	givenSetup := Given("the setup", func() { testLabel = "GIVEN the setup" })
+	moreSetup := Given("more setup", func() { testLabel += " AND GIVEN more setup" })
+	somethingHappens := When("something happens", func() { testLabel += " WHEN something happens" })
+	somethingElseHappens := When("something else happens", func() { testLabel += " AND WHEN something else happens" })
+	assertSomething := Then("assert something", func(t *testing.T) {
+		testLabel += " THEN assert something"
+		assertNameEquals(t, testLabel)
+	})
+	assertSomethingElse := Then("assert something else", func(t *testing.T) {
+		testLabel += " THEN assert something else"
+		assertNameEquals(t, testLabel)
+	})
+
+	givenSetup.Given2(moreSetup).When2(somethingHappens).Then2(assertSomething).Run(t)
+	givenSetup.Branch(
+		somethingHappens.Then2(assertSomething),
+		moreSetup.When2(somethingHappens).When2(somethingElseHappens).Then2(assertSomethingElse),
+	).Run(t)
 }
 
 func assertNameEquals(t *testing.T, testLabel string) bool {
