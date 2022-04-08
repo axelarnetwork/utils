9473fc8c29b4fc82377bf98b4a5ee9efb9875204
Christian Gorenflo <christian@axelar.network>
2022-04-07 22:44:10 -0400

    fix(slices): use len instead of cap in Map (#25)

--- slices/slices.go
+++ slices/slices.go
@@ -2,7 +2,7 @@ package slices
 
 // Map maps a slice of T to a slice of S
 func Map[T, S any](source []T, f func(T) S) []S {
-	out := make([]S, cap(source))
+	out := make([]S, len(source))
 
 	// avoid allocating a copy of the slice element
 	for i := range source {
