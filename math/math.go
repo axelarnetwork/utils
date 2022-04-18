320215092f5b04e62ec500a9330f94ebc48fac31
Christian Gorenflo <christian@axelar.network>
2022-04-18 16:36:49 -0400

    lint

--- math/math.go
+++ math/math.go
@@ -2,6 +2,7 @@ package math
 
 import "golang.org/x/exp/constraints"
 
+// Max returns the bigger of two numbers
 func Max[T constraints.Ordered](first, second T) T {
 	if first > second {
 		return first
@@ -9,6 +10,7 @@ func Max[T constraints.Ordered](first, second T) T {
 	return second
 }
 
+// Min returns the smaller of two numbers
 func Min[T constraints.Ordered](first, second T) T {
 	if first > second {
 		return second
