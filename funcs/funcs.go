3af8582478582cb9965628f64cfb446a0a277cd8
Christian Gorenflo <christian@axelar.network>
2022-06-30 15:49:21 -0400

    linting

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -12,6 +12,7 @@ func Compose[T1, T2, T3 any](f func(T1) T2, g func(T2) T3) func(T1) T3 {
 // Identity returns the given element
 func Identity[T any](x T) T { return x }
 
+// Must returns the result if err is nil, panics otherwise
 func Must[T any](result T, err error) T {
 	if err != nil {
 		panic(errors.Wrap(err, "call should not have failed"))
