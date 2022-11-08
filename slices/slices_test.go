29d8b6fc05c2c4d7b4ec389fa85319c7ab884bb2
Milap Sheth <milap@axelar.network>
2022-11-08 18:36:36 -0500

    feat: add duplicate checker for slices (#66)
    * feat: add duplicate checker for slices
    
    * fix test
    
    * add edge case

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -150,6 +150,12 @@ func TestDistinct(t *testing.T) {
 	assert.Equal(t, []int{0, 3, 2, 7, 1}, out)
 }
 
+func TestHasDuplicates(t *testing.T) {
+	assert.True(t, slices.HasDuplicates([]int{0, 3, 2, 7, 2, 1, 3, 0}))
+	assert.False(t, slices.HasDuplicates([]int{0, 3, 2, 7, 1}))
+	assert.False(t, slices.HasDuplicates([]int{}))
+}
+
 func TestToMap(t *testing.T) {
 	source := []int{0, 3, 2, 7, 3}
 	m := slices.ToMap(source, strconv.Itoa)
