b614f3c8f700c907a3bee728e74f40bae0bbec78
Christian Gorenflo <christian@axelar.network>
2022-11-16 10:30:24 -0500

    feat(maps): add Filter function (#68)

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -14,3 +14,12 @@ func TestHas(t *testing.T) {
 	assert.True(t, maps.Has(m, 3))
 	assert.False(t, maps.Has(m, 10))
 }
+
+func TestFilter(t *testing.T) {
+	m := map[int]string{
+		1: "foo",
+		2: "bar",
+		3: "test"}
+
+	assert.EqualValues(t, map[int]string{2:"bar"}, maps.Filter(m, func(k int, v string) bool { return k !=1 && v != "test" }))
+}
