71a5f958c46497cffeafd8e8c7cfa9d27bd420df
Christian Gorenflo <christian@axelar.network>
2023-07-06 00:34:45 -0400

    adjust backoff algorithm

--- backoff.go
+++ backoff.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"github.com/axelarnetwork/utils/math"
 	"math/rand"
 	"time"
 )
@@ -12,9 +11,13 @@ type BackOff func(currentRetryCount int) time.Duration
 // ExponentialBackOff computes an exponential back-off
 func ExponentialBackOff(minTimeout time.Duration) BackOff {
 	return func(currentRetryCount int) time.Duration {
-		jitter := rand.Float64() + 0.5
-		strategy := 1 << currentRetryCount
-		backoff := math.Max(jitter*float64(strategy)*float64(minTimeout.Nanoseconds()), float64(minTimeout.Nanoseconds()))
+		jitter := rand.Float64()
+		jitterMax := 200 * time.Millisecond
+		if currentRetryCount < 1 {
+			currentRetryCount = 1
+		}
+		strategy := 1 << (currentRetryCount - 1)
+		backoff := (float64(strategy) * float64(minTimeout.Nanoseconds())) + (jitter * float64(jitterMax.Nanoseconds()))
 
 		return time.Duration(backoff)
 	}
@@ -23,8 +26,9 @@ func ExponentialBackOff(minTimeout time.Duration) BackOff {
 // LinearBackOff computes a linear back-off
 func LinearBackOff(minTimeout time.Duration) BackOff {
 	return func(currentRetryCount int) time.Duration {
-		jitter := rand.Float64() + 0.5
-		backoff := math.Max(jitter*float64(currentRetryCount)*float64(minTimeout.Nanoseconds()), float64(minTimeout.Nanoseconds()))
+		jitter := rand.Float64()
+		jitterMax := 200 * time.Millisecond
+		backoff := (float64(currentRetryCount) * float64(minTimeout.Nanoseconds())) + (jitter * float64(jitterMax.Nanoseconds()))
 		return time.Duration(backoff)
 	}
 }
